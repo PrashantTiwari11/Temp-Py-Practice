500a2fb965fda574e960ab4a5d1cf29a30c19113
PRASHANT TIWARI <pt867987@gmail.com>
2026-09-01 19:29:52 +0530

    Python-Practice17

---
+++ Python-Practice17.py
@@ -0,0 +1,32 @@
+# Exception Handling
+
+# Question 1
+print('Program 1 - Exception Handling')
+
+# Question 2
+print('Program 2 - Exception Handling')
+
+# Question 3
+print('Program 3 - Exception Handling')
+
+# Question 4
+print('Program 4 - Exception Handling')
+
+# Question 5
+print('Program 5 - Exception Handling')
+
+# Question 6
+print('Program 6 - Exception Handling')
+
+# Question 7
+print('Program 7 - Exception Handling')
+
+# Question 8
+print('Program 8 - Exception Handling')
+
+# Question 9
+print('Program 9 - Exception Handling')
+
+# Question 10
+print('Program 10 - Exception Handling')
+
