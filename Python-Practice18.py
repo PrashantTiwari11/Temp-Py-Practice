6c622d082c2da917e16e60fd49d9e15842356a4d
PRASHANT TIWARI <pt867987@gmail.com>
2026-09-01 19:30:11 +0530

    Python-Practice18

---
+++ Python-Practice18.py
@@ -0,0 +1,32 @@
+# Object Oriented Programming
+
+# Question 1
+print('Program 1 - Object Oriented Programming')
+
+# Question 2
+print('Program 2 - Object Oriented Programming')
+
+# Question 3
+print('Program 3 - Object Oriented Programming')
+
+# Question 4
+print('Program 4 - Object Oriented Programming')
+
+# Question 5
+print('Program 5 - Object Oriented Programming')
+
+# Question 6
+print('Program 6 - Object Oriented Programming')
+
+# Question 7
+print('Program 7 - Object Oriented Programming')
+
+# Question 8
+print('Program 8 - Object Oriented Programming')
+
+# Question 9
+print('Program 9 - Object Oriented Programming')
+
+# Question 10
+print('Program 10 - Object Oriented Programming')
+
