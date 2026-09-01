5e9397b1472d50948d1065a09fabf796174b7d7b
PRASHANT TIWARI <pt867987@gmail.com>
2026-09-01 19:29:30 +0530

    Python-Practice16

---
+++ Python-Practice16.py
@@ -0,0 +1,32 @@
+# File Handling
+
+# Question 1
+print('Program 1 - File Handling')
+
+# Question 2
+print('Program 2 - File Handling')
+
+# Question 3
+print('Program 3 - File Handling')
+
+# Question 4
+print('Program 4 - File Handling')
+
+# Question 5
+print('Program 5 - File Handling')
+
+# Question 6
+print('Program 6 - File Handling')
+
+# Question 7
+print('Program 7 - File Handling')
+
+# Question 8
+print('Program 8 - File Handling')
+
+# Question 9
+print('Program 9 - File Handling')
+
+# Question 10
+print('Program 10 - File Handling')
+
