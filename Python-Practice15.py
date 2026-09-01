cda5156f6eb3d1f5aa76951138cebc255b306a15
PRASHANT TIWARI <pt867987@gmail.com>
2026-09-01 19:29:07 +0530

    Python-Practice15

---
+++ Python-Practice15.py
@@ -0,0 +1,32 @@
+# Advanced Functions
+
+# Question 1
+print('Program 1 - Advanced Functions')
+
+# Question 2
+print('Program 2 - Advanced Functions')
+
+# Question 3
+print('Program 3 - Advanced Functions')
+
+# Question 4
+print('Program 4 - Advanced Functions')
+
+# Question 5
+print('Program 5 - Advanced Functions')
+
+# Question 6
+print('Program 6 - Advanced Functions')
+
+# Question 7
+print('Program 7 - Advanced Functions')
+
+# Question 8
+print('Program 8 - Advanced Functions')
+
+# Question 9
+print('Program 9 - Advanced Functions')
+
+# Question 10
+print('Program 10 - Advanced Functions')
+
